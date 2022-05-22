a3c04b957f74c1bbf310a0abef44e19b4b11aae7
Leonetienne <leon@etiennes.de>
2022-05-22 17:24:56 +0200

    Implemented a Key::Random() method

--- GCryptLib/src/Key.cpp
+++ GCryptLib/src/Key.cpp
@@ -1,6 +1,8 @@
 #include "GCrypt/Key.h"
 #include "GCrypt/GHash.h"
 #include "GCrypt/Util.h"
+#include <random>
+#include <cassert>
 
 namespace Leonetienne::GCrypt {
 
@@ -8,6 +10,24 @@ namespace Leonetienne::GCrypt {
     return GHash::CalculateHashsum(
       StringToBits(password)
     );
+  }
+
+  Key Key::Random() {
+    // Create our truly-random rng
+    std::random_device rng;
+    constexpr std::size_t bitsPerCall = sizeof(std::random_device::result_type) * 8;
+
+    // Fetch BLOCK_SIZE bits
+    std::stringstream ss;
+    for (std::size_t i = 0; i < BLOCK_SIZE / bitsPerCall; i++) {
+      ss << std::bitset<bitsPerCall>(rng());
+    }
+
+    // Verify that we actually have the correct size
+    assert(ss.str().length() == BLOCK_SIZE);
+
+    // Return them as a key
+    return Key(Block(ss.str()));
   }
 
   Key::Key() : Block() {
