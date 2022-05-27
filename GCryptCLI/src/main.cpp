aac31ea5dc2112f91d9d555dee36bdeee2cfd9b5
Leonetienne <leon@etiennes.de>
2022-05-27 16:12:41 +0200

    Removed bunch of garbage from main.cpp

--- GCryptCLI/src/main.cpp
+++ GCryptCLI/src/main.cpp
@@ -1,10 +1,6 @@
 #include "CommandlineInterface.h"
 #include "Configuration.h"
 #include "ModulePrepareKey.h"
-#include "ModuleDataFormatter.h"
-#include "Bases.h"
-#include <iostream>
-#include <GCrypt/Util.h>
 
 int main(int argc, char* const* argv) {
 
@@ -16,88 +12,6 @@ int main(int argc, char* const* argv) {
 
   // Prepare the key
   ModulePrepareKey::PrepareKey();
-
-  const std::vector<Block> textblocks = StringToBitblocks("Hansel and Gretel are a brother and sister abandoned in a forest, where they fall into the hands of a witch who lives in a house made of gingerbread, cake, and candy. The cannibalistic witch intends to fatten the children before eventually eating them, but Gretel outwits the witch and kills her. The two children then escape with their lives and return home with the witch's treasure.");
-  const std::string formattedBlocks =
-    ModuleDataFormatter::FormatBlocks(
-      textblocks,
-      Configuration::iobaseFormat
-    );
-
-  std::cout << "Formatted: " << formattedBlocks << std::endl << std::endl;
-
-  const std::vector<Block> retrievedBlocks =
-    ModuleDataFormatter::DecodeFormatMultiblock(
-      formattedBlocks,
-      Configuration::iobaseFormat
-    );
-
-  const std::string retrievedText = BitblocksToString(retrievedBlocks);
-  std::cout << "Retrieved: " << retrievedText << std::endl << std::endl;
-
-  return 0;
-
-  /*
-  Block all1;
-  for (std::size_t i = 0; i < 16; i++)
-    all1[i] = -1;
-
-  std::cout
-    << ModuleDataFormatter::FormatBlock(
-        all1,
-         Configuration::iobaseFormat
-       )
-    << std::endl
-    << std::endl;
-
-  std::cout
-    << ModuleDataFormatter::FormatBlock(
-        all1,
-         Configuration::iobaseFormat
-       ).size()
-    << std::endl
-    << std::endl;
-  */
-
-  /*
-  std::cout
-    << ModuleDataFormatter::FormatBlock(
-         //ModulePrepareKey::GetKey(),
-        block,
-         Configuration::iobaseFormat
-       )
-    << std::endl
-    << std::endl;
-    */
-
-
-  /*
-  const std::string customBase =
-    ModuleDataFormatter::FormatBlock(
-      block,
-      Configuration::iobaseFormat
-  );
-
-  std::cout << "\"" << customBase << "\"" << std::endl << std::endl;
-
-  const Block back =
-    ModuleDataFormatter::StringToBlock(
-      customBase,
-      Configuration::iobaseFormat
-  );
-
-  std::cout << back.ToHexString() << std::endl << std::endl;
-  */
-
-  std::cout
-    << ModuleDataFormatter::DecodeFormat(
-        ModuleDataFormatter::FormatBlock(
-          ModulePrepareKey::GetKey(),
-          Configuration::iobaseFormat
-        ),
-        Configuration::iobaseFormat
-      ).ToTextString()
-    << std::endl;
 
   return 0;
 }
