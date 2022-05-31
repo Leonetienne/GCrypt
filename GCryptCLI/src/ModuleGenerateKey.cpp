ba37b4325c8eefbff8bbd508b0487d7e14573e22
Leonetienne <leon@etiennes.de>
2022-05-31 18:19:12 +0200

    Integrated DataOutputLayer to ModuleGenerateKey

--- GCryptCLI/src/ModuleGenerateKey.cpp
+++ GCryptCLI/src/ModuleGenerateKey.cpp
@@ -1,5 +1,5 @@
 #include "ModuleGenerateKey.h"
-
+#include "DataOutputLayer.h"
 #include "KeyManager.h"
 #include "CommandlineInterface.h"
 
@@ -8,7 +8,24 @@ using namespace Module;
 
 void GenerateKey::Run() {
 
-  // Pass KeyManager::GetKey() to data output layer
+  // Initialize the data output layer
+  IO::DataOutputLayer::Init();
+
+  // Enqueue our single block of data
+  IO::DataOutputLayer::Enqueue(KeyManager::GetKey());
+
+  // Tell the data output layer, that is has received all blocks
+  IO::DataOutputLayer::ReachedEOF();
+
+  // Tell it to write all blocks
+  // (a single call should suffice, but a while-loop is the proper
+  //  way to do it)
+  while (!IO::DataOutputLayer::IsFinished()) {
+    IO::DataOutputLayer::WriteBlock();
+  }
+
+  // Destruct the data output layer
+  IO::DataOutputLayer::Destruct();
 
   return;
 }
